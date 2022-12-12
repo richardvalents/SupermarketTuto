23e1a20384d4c249578f3f39ed963d35ac397b17
HP <HP@LAPTOP-9K9B10GQ>
2022-12-12 22:53:12 +0700

    Add project files.

---
+++ SupermarketTuto/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace SupermarketTuto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string Sellername = "";   
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\Supermarket\SupermarketTuto\smarketdb.mdf;Integrated Security=True;Connect Timeout=30");
+        private void bunifuMaterialTextbox2_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            UnameTb.Text = "";
+            PassTb.Text = "";
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            if(UnameTb.Text=="" || PassTb.Text == "")
+            {
+                MessageBox.Show("Mohon Masukkan Username & Password");
+            }
+            else
+            {
+                if (RoleCb.SelectedIndex > -1)
+                {
+                    if (RoleCb.SelectedItem.ToString() == "ADMIN")
+                    {
+                        if (UnameTb.Text == "Admin" && PassTb.Text == "Admin")
+                        {
+                            ProductForm prod = new ProductForm();
+                            prod.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Jika anda Admin, Mohon masukkan Username dan Password!");
+                        }
+                    }
+                    else
+                    {
+                        //MessageBox.Show("Your In The Seller Section ");
+                        Con.Open();
+                        SqlDataAdapter sda = new SqlDataAdapter("Select count(8) from SellerTbl where SellerName='" + UnameTb.Text + "' and SellerPass='" + PassTb.Text + "'", Con);
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        if(dt.Rows[0][0].ToString() == "1")
+                        {
+                            Sellername = UnameTb.Text;
+                            SellingForm sell = new SellingForm();
+                            sell.Show();
+                            this.Hide();
+                            Con.Close();
+                           
+                        }
+                        else
+                        {
+                            MessageBox.Show("Password/Username Salah!");
+                        }
+                        Con.Close();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Mohon Pilih Sebuah Role!");
+                }
+            }
+        }
+
+        private void RoleCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void RoleCb_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
